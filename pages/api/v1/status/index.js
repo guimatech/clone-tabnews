f3b122b17bc9885aacfb0701eb548c59f0bb857e
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2023-12-19 02:58:46 +0000

    feat: finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,29 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as rows;');
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+  const databaseVersion = (await database.query("SHOW server_version;")).rows[0].server_version;
+  const databaseMaxConnections = (await database.query("SHOW max_connections;"))
+    .rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = (await database.query({
+    text: "SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  }))
+    .rows[0].opened_connections
+  
   response
     .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+    .json({ 
+      update_at: updateAt,
+      dependencies: {
+        database: {
+          version: databaseVersion,
+          max_connections: parseInt(databaseMaxConnections),
+          opened_connections: databaseOpenedConnections,
+        }
+      }
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,51 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  let response;
+  let responseBody;
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    responseBody = await response.json();
+    return { update_at, dependencies } = responseBody;
+  });
+
+  test("should return 200", () => {
+    expect(response.status).toBe(200);
+  });
+
+  test("should return object containing", () => {
+    expect(responseBody).toMatchObject(
+      expect.objectContaining({ 
+        update_at: expect.any(String),
+        dependencies: {
+          database: {
+            version: expect.any(String),
+            max_connections: expect.any(Number),
+            opened_connections: expect.any(Number)
+          }
+        }
+      })
+    );
+  });
+
+  test("should return `update_at` with a valid date on pattern ISO 8601", () => {
+    expect(update_at).toBeDefined();
+    const parsedUpdateAt = new Date(update_at).toISOString();
+    expect(update_at).toEqual(parsedUpdateAt);
+  });
+
+  test("should return `version` equal 16.0", () => {
+    expect(dependencies.database.version).toEqual("16.0")
+  });
+
+  test("should return `max_connections` equal 100", () => {
+    expect(dependencies.database.max_connections).toBe(100);
+  });
+
+  test("should return `opened_connections` equal 1 ", () => {
+    expect(dependencies.database.opened_connections).toBe(1)
+  });
+
+  test("should return `max_connections` greater than `opened_connections`", () => {
+    expect(dependencies.database.max_connections)
+      .toBeGreaterThan(dependencies.database.opened_connections);
+  });
 });
