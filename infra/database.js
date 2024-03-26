897e2532e07b49198e9bb05cdf4cab658735e69a
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2024-03-26 17:39:26 -0300

    feat: add migrations and setup environment

--- infra/database.js
+++ infra/database.js
@@ -1,37 +1,37 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      ssl: getSSLValues()
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
 
-    try {
-      await client.connect();
-      const result = await client.query(queryObject);
-      return result;
-    } catch(error) {
-      console.error(error);
-      throw error;
-    } finally {
-      await client.end();
-    }
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-    query: query,
-}
+  query: query,
+};
 
 function getSSLValues() {
-    if (process.env.POSTGRES_CA) {
-        return {
-          ca: process.env.POSTGRES_CA
-        };
-    }
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
 
-    return process.env.NODE_ENV !== 'development';
+  return process.env.NODE_ENV === "production";
 }
