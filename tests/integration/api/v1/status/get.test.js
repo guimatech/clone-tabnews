1040465ecdc8fcbba3209ef98e4158e7fd79999a
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2024-04-09 22:21:16 -0300

    test: fix status test

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,10 +1,9 @@
 describe("GET to /api/v1/status", () => {
   let response;
   let responseBody;
-  beforeAll(async () => {
-    response = await fetch("http://localhost:3001/api/v1/status");
+  beforeEach(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
     responseBody = await response.json();
-    return ({ update_at, dependencies } = responseBody);
   });
 
   test("should return 200", () => {
@@ -27,26 +26,26 @@ describe("GET to /api/v1/status", () => {
   });
 
   test("should return `update_at` with a valid date on pattern ISO 8601", () => {
-    expect(update_at).toBeDefined();
-    const parsedUpdateAt = new Date(update_at).toISOString();
-    expect(update_at).toEqual(parsedUpdateAt);
+    expect(responseBody.update_at).toBeDefined();
+    const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+    expect(responseBody.update_at).toEqual(parsedUpdateAt);
   });
 
   test("should return `version` equal 16.0", () => {
-    expect(dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
   });
 
   test("should return `max_connections` equal 100", () => {
-    expect(dependencies.database.max_connections).toBe(100);
+    expect(responseBody.dependencies.database.max_connections).toBe(100);
   });
 
   test("should return `opened_connections` equal 1 ", () => {
-    expect(dependencies.database.opened_connections).toBe(1);
+    expect(responseBody.dependencies.database.opened_connections).toBe(1);
   });
 
   test("should return `max_connections` greater than `opened_connections`", () => {
-    expect(dependencies.database.max_connections).toBeGreaterThan(
-      dependencies.database.opened_connections,
+    expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(
+      responseBody.dependencies.database.opened_connections,
     );
   });
 });
