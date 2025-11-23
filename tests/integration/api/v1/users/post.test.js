f97454ada6bb90936fb05c13b0712adcaa1d5103
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2025-11-23 14:07:25 +0000

    feat: add `features` column `user` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -31,6 +31,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "lucasguimaraes",
         email: "guimaraes-lucas@outlook.com.br",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
